26073b116ccab8f396fa3f474c8f947069fe8911
patrick.heenan@colorado.edu <patrick.heenan@colorado.edu>
2018-03-09 16:29:18 -0700

    functionalized getting expected.

--- Testing/MainTesting.py
+++ Testing/MainTesting.py
@@ -36,6 +36,17 @@ def check_losses(expected,predicted,atol,max_rel_loss=0.0137,rtol=2e-2):
     assert loss_rel < max_rel_loss
     np.testing.assert_allclose(predicted,expected,atol=atol,rtol=rtol)
 
+def expected_bidirectional(data_base,q_predicted):
+    bidir = np.loadtxt(data_base + "data_bidir.csv",delimiter=",")
+    expected = bidir
+    ext_fwd_m, G_fwd_J = expected[:,0] * 1e-9, expected[:,1]*4.1e-21
+    # interpolate the expected landscape onto the actual one
+    interp_expected = interp1d(x=ext_fwd_m, y=G_fwd_J, kind='linear',
+                               fill_value='extrapolate',bounds_error=False)
+    G0_expected = interp_expected(q_predicted)
+    G0_expected -= np.min(G0_expected)
+    return G0_expected
+
 def run():
     fwd,rev = Test.HummerData(n=100)
     fwd_wham = to_wham(fwd)
@@ -44,14 +55,7 @@ def run():
                                            rev_input=rev_wham)
     q = wham_landcape.q
     data_base = "../data/"
-    bidir = np.loadtxt(data_base + "data_bidir.csv",delimiter=",")
-    expected = bidir
-    ext_fwd_m, G_fwd_J = expected[:,0] * 1e-9, expected[:,1]*4.1e-21
-    # interpolate the expected landscape onto the actual one
-    interp_expected = interp1d(x=ext_fwd_m, y=G_fwd_J, kind='linear',
-                               fill_value='extrapolate',bounds_error=False)
-    G0_expected = interp_expected(wham_landcape.q)
-    G0_expected -= min(G0_expected)
+    G0_expected = expected_bidirectional(data_base,q)
     G0_WHAM = wham_landcape.G0
     G0_WHAM -= min(G0_WHAM)
     kT = 1/wham_landcape.beta
